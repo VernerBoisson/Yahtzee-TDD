079bdbc93c285a729b81f160516d37a565bdffdb
VernerBoisson <verner.boisson@gmail.com>
2022-04-22 16:22:47 +0200

    define all functions

--- YAHTZEE/game.py
+++ YAHTZEE/game.py
@@ -1,9 +1,38 @@
-
-# function to return ones result
 def ones():
     return 0
 
-# function to return twos result
 def twos():
     return 0
 
+def threes():
+    return 0
+
+def fours():
+    return 0
+
+def fives():
+    return 0
+
+def sixes():
+    return 0
+
+def three_of_a_kind():
+    return 0
+
+def four_of_a_kind():
+    return 0
+
+def full_house():
+    return 0
+
+def small_straight():
+    return 0
+
+def large_straight():
+    return 0
+
+def chance():
+    return 0
+
+def yahtzee():
+    return 0
