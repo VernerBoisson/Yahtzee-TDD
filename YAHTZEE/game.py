e467cac631d2b9644b7f6da3302c9ce40cc6104b
VernerBoisson <verner.boisson@gmail.com>
2022-04-23 20:40:22 +0200

    refactor straight

--- YAHTZEE/game.py
+++ YAHTZEE/game.py
@@ -13,17 +13,19 @@ def find_number_of_a_kind(dice_tuple, number_of_a_kind):
             return sum(dice_tuple)
     return 0
 
-def full_house():
+def full_house(dice_tuple):
+    if dice_tuple.count(dice_tuple[0]) == 2 and dice_tuple.count(dice_tuple[1]) == 3:
+        return 25
     return 0
 
-def small_straight(dice_tuple):
-    if all(i in dice_tuple for i in [1, 2, 3, 4]) or \
-        all(i in dice_tuple for i in [2, 3, 4, 5]) or\
-        all(i in dice_tuple for i in [3, 4, 5, 6]):
-        return 30
+def find_straight(dice_tuple, straight_type):
+    if straight_type == 'small':
+        if all(i in dice_tuple for i in [1, 2, 3, 4]) or \
+            all(i in dice_tuple for i in [2, 3, 4, 5]) or \
+            all(i in dice_tuple for i in [3, 4, 5, 6]):
+            return 30
+    elif straight_type == 'large':
+        if all(i in dice_tuple for i in [1, 2, 3, 4, 5]) or \
+            all(i in dice_tuple for i in [2, 3, 4, 5, 6]):
+            return 40
     return 0
-
-def large_straight(dice_tuple):
-    if all(i in dice_tuple for i in [1, 2, 3, 4, 5]) or all(i in dice_tuple for i in [2, 3, 4, 5, 6]):
-        return 40
-    return 0    
